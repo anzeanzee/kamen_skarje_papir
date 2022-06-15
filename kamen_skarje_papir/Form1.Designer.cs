2720ef52f91fd7c9132f570e340265c6b6e85169
Anže Lesjak <anze.lesjak@scv.si>
2022-06-15 19:45:33 +0200

    first commit

---
+++ kamen_skarje_papir/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace kamen_skarje_papir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtCountDown = new System.Windows.Forms.Label();
+            this.txtRounds = new System.Windows.Forms.Label();
+            this.izbere_comp = new System.Windows.Forms.PictureBox();
+            this.izbere_igralec = new System.Windows.Forms.PictureBox();
+            this.countDownTimer = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.izbere_comp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.izbere_igralec)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(71, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Rock";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(71, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Paper";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Location = new System.Drawing.Point(71, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Scissor";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.btnScissors_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(679, 343);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 37);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "restart";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(277, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Jaz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(616, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Comp";
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtScore.Location = new System.Drawing.Point(412, 99);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(124, 18);
+            this.txtScore.TabIndex = 8;
+            this.txtScore.Text = "Jaz:0 - Comp:0";
+            // 
+            // txtCountDown
+            // 
+            this.txtCountDown.AutoSize = true;
+            this.txtCountDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtCountDown.Location = new System.Drawing.Point(462, 166);
+            this.txtCountDown.Name = "txtCountDown";
+            this.txtCountDown.Size = new System.Drawing.Size(33, 36);
+            this.txtCountDown.TabIndex = 9;
+            this.txtCountDown.Text = "5";
+            this.txtCountDown.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtRounds
+            // 
+            this.txtRounds.AutoSize = true;
+            this.txtRounds.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtRounds.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtRounds.Location = new System.Drawing.Point(437, 305);
+            this.txtRounds.Name = "txtRounds";
+            this.txtRounds.Size = new System.Drawing.Size(99, 25);
+            this.txtRounds.TabIndex = 10;
+            this.txtRounds.Text = "Round: 3";
+            // 
+            // izbere_comp
+            // 
+            this.izbere_comp.Image = global::kamen_skarje_papir.Properties.Resources.vprasaj;
+            this.izbere_comp.Location = new System.Drawing.Point(567, 82);
+            this.izbere_comp.Name = "izbere_comp";
+            this.izbere_comp.Size = new System.Drawing.Size(170, 163);
+            this.izbere_comp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.izbere_comp.TabIndex = 5;
+            this.izbere_comp.TabStop = false;
+            this.izbere_comp.Click += new System.EventHandler(this.izbere_comp_Click);
+            // 
+            // izbere_igralec
+            // 
+            this.izbere_igralec.Image = global::kamen_skarje_papir.Properties.Resources.vprasaj;
+            this.izbere_igralec.Location = new System.Drawing.Point(220, 82);
+            this.izbere_igralec.Name = "izbere_igralec";
+            this.izbere_igralec.Size = new System.Drawing.Size(168, 163);
+            this.izbere_igralec.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.izbere_igralec.TabIndex = 4;
+            this.izbere_igralec.TabStop = false;
+            // 
+            // countDownTimer
+            // 
+            this.countDownTimer.Interval = 1000;
+            this.countDownTimer.Tick += new System.EventHandler(this.countDownTimerEvent);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(45, 320);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(98, 30);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "music";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(45, 359);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 31);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "stop m";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.txtRounds);
+            this.Controls.Add(this.txtCountDown);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.izbere_comp);
+            this.Controls.Add(this.izbere_igralec);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.izbere_comp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.izbere_igralec)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox izbere_igralec;
+        private System.Windows.Forms.PictureBox izbere_comp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtCountDown;
+        private System.Windows.Forms.Label txtRounds;
+        private System.Windows.Forms.Timer countDownTimer;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ kamen_skarje_papir/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace kamen_skarje_papir
+{
+    public partial class Form1 : Form
+    {
+        int rounds = 3;
+        int timerPerRound = 6;
+        bool gameover = false;
+        string[] CompchoiseList = { "rock", "paper","scissor", "rock", "paper", "scissor" };
+
+        int randomNumber = 0;
+
+        Random rnd = new Random();
+
+        string CompChoise;
+
+        string JazChoise;
+
+        int JazScore;
+        int CompScore;
+
+        public Form1()
+        {
+            InitializeComponent();
+            countDownTimer.Enabled = true;
+
+            JazChoise = "none";
+
+            txtCountDown.Text = "5";
+        }
+
+        private void izbere_comp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            JazScore = 0;
+            CompScore = 0;
+            rounds = 3;
+            txtScore.Text = "Jaz:" + JazScore + " - " + "Comp:" + CompScore;
+            JazChoise = "none";
+            countDownTimer.Enabled = true;
+            izbere_igralec.Image = Properties.Resources.vprasaj;
+            izbere_comp.Image = Properties.Resources.vprasaj;
+            gameover = false;
+        }
+
+        private void countDownTimerEvent(object sender, EventArgs e)
+        {
+            timerPerRound -= 1;
+            txtCountDown.Text = timerPerRound.ToString();
+            txtRounds.Text = "Rounds: " + rounds;
+
+            if (timerPerRound < 1)
+            {
+                countDownTimer.Enabled = false;
+                timerPerRound = 6;
+                randomNumber = rnd.Next(0, CompchoiseList.Length);
+                CompChoise = CompchoiseList[randomNumber];
+
+                switch (CompChoise)
+                {
+                    case "rock":
+                        izbere_comp.Image = Properties.Resources.rock;
+                        break;
+                    case "paper":
+                        izbere_comp.Image = Properties.Resources.paper;
+                        break;
+                    case "scissor":
+                        izbere_comp.Image = Properties.Resources.Screenshot_2;
+                        break;
+                }
+
+                if (rounds > 0)
+                {
+                    checkGame();
+                }
+                else
+                {
+                    if (JazScore > CompScore)
+                    {
+                        MessageBox.Show("Zmagal si igro!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Comp je zmagal igro");
+                    }
+                    gameover = true;
+                }                             
+                
+            }
+        }
+
+        private void checkGame()
+        {
+            if (JazChoise == "rock" && CompChoise == "paper")
+            {
+                CompScore += 1;
+                rounds -= 1;
+
+                MessageBox.Show("Zmaga Comp, papir premaga škarje");
+            }
+            else if (JazChoise == "scissor" && CompChoise == "rock")
+            {
+                CompScore += 1;
+                rounds -= 1;
+
+                MessageBox.Show("Zmaga Comp, škarje zgubijo proti kamnu");
+            }
+            else if (JazChoise == "paper" && CompChoise == "scissor")
+            {
+                CompScore += 1;
+                rounds -= 1;
+
+                MessageBox.Show("Zmaga Comp, papir zgubi proti škarjam");
+            }
+            else if (JazChoise == "paper" && CompChoise == "rock")
+            {
+                JazScore += 1;
+                rounds -= 1;
+
+                MessageBox.Show("Zmaga igralec, papir zmaga kamen");
+            }
+            else if (JazChoise == "scissor" && CompChoise == "paper")
+            {
+                JazScore += 1;
+                rounds -= 1;
+
+                MessageBox.Show("Zmaga igralec, škarje zmagajo papir");
+            }
+            else if (JazChoise == "rock" && CompChoise == "scissor")
+            {
+                JazScore += 1;
+                rounds -= 1;
+
+                MessageBox.Show("Zmaga igralec, kamen zmaga škarje");
+            }
+            else if (JazChoise == "none")
+            {
+                MessageBox.Show("izberi nekaj");
+            }
+            else
+            {
+                MessageBox.Show("izenačeno");
+            }
+            startNextRound();
+
+        }
+
+        private void startNextRound()
+        {
+            if(gameover == true)
+            {
+                return;
+            }
+            txtScore.Text = "Jaz:" + JazScore + " - " + "Comp:" + CompScore;
+            JazChoise = "none";
+            countDownTimer.Enabled = true;
+            izbere_igralec.Image = Properties.Resources.vprasaj;
+            izbere_comp.Image = Properties.Resources.vprasaj;
+        }
+
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            izbere_igralec.Image = Properties.Resources.rock;
+            JazChoise = "rock";
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            izbere_igralec.Image = Properties.Resources.paper;
+            JazChoise = "paper";
+        }
+
+        private void btnScissors_Click(object sender, EventArgs e)
+        {
+            izbere_igralec.Image = Properties.Resources.Screenshot_2;
+            JazChoise = "scissor";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SoundPlayer splayer = new SoundPlayer(@"C:\Users\uroš\Downloads\ShBoom.wav");
+            splayer.Play();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SoundPlayer splayer = new SoundPlayer(@"C:\Users\uroš\Downloads\ShBoom.wav");
+            splayer.Stop();
+        }
+    }
+}
